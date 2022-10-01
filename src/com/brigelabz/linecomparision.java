2827d4956bfaef4416c63045dd0b6835bc40ea44
punamr <punamjondhale1994@gmail.com>
2022-09-30 23:31:54 -0700

    Added:Check equality of two lines

--- src/com/brigelabz/linecomparision.java
+++ src/com/brigelabz/linecomparision.java
@@ -4,26 +4,45 @@ import java.util.Scanner;
 
 public class linecomparision {
     public static int lineLengthCal() {
-
     int x1,y1,x2,y2;
-    Scanner scn = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
     System.out.println("Enter the co-ordinates of line!!: x1,y1,x2,y2");
-    x1 = scn.nextInt();
-    y1 = scn.nextInt();
-    x2 = scn.nextInt();
-    y2 = scn.nextInt();
-    int lineLength =(int)Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );
+    x1 = scanner.nextInt();
+    y1 = scanner.nextInt();
+    x2 = scanner.nextInt();
+    y2 = scanner.nextInt();
+    int lineLength = (int)Math.sqrt(Math.pow((x2-x1), 2)
+            + Math.pow((y2-y1), 2) );
     return lineLength;
 
 }
+    public static void equalityCheck
+            (int lineOneLength , int lineTwoLength ) {
+        if(lineOneLength == lineTwoLength ) {
+            System.out.println("Lines are Equal");
+        }
+        else {
+            System.out.println("Lines are not Equal");
+        }
+    }
 
     public static void main(String[] args) {
+        int lineOneLength = lineLengthCal();
+        int lineTwoLength = lineLengthCal();
+        System.out.println("The length of line 1 is: "+lineOneLength);
+        System.out.println("The length of line 2 is: "+lineTwoLength);
+        equalityCheck(lineOneLength,lineTwoLength);
 
-        int lineLength = lineLengthCal();
-        System.out.println("The length of line is: "+lineLength);
     }
 
 }
+
+
+
+
+
+
+
 
 
 
