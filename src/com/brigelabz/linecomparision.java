92640774af6dcaea67169abc251c0372e7e277ff
punamr <punamjondhale1994@gmail.com>
2022-09-30 23:19:50 -0700

    Added:Calculate the length of two point

--- src/com/brigelabz/linecomparision.java
+++ src/com/brigelabz/linecomparision.java
@@ -1,8 +1,32 @@
 package com.brigelabz;
 
-public class linecomparision {
-    public static void main(String[] args) {
-        System.out.println("Welcome to line Comparision computation program");
+import java.util.Scanner;
 
-    }
+public class linecomparision {
+    public static int lineLengthCal() {
+
+    int x1,y1,x2,y2;
+    Scanner scn = new Scanner(System.in);
+    System.out.println("Enter the co-ordinates of line!!: x1,y1,x2,y2");
+    x1 = scn.nextInt();
+    y1 = scn.nextInt();
+    x2 = scn.nextInt();
+    y2 = scn.nextInt();
+    int lineLength =(int)Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );
+    return lineLength;
+
 }
+
+    public static void main(String[] args) {
+
+        int lineLength = lineLengthCal();
+        System.out.println("The length of line is: "+lineLength);
+    }
+
+}
+
+
+
+
+
+
