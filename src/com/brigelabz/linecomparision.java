1db1891cce299f0bc5582278bed1d505afec42de
punamr <punamjondhale1994@gmail.com>
2022-09-30 23:10:20 -0700

    Added:Welcome to line comparision computation program

---
+++ src/com/brigelabz/linecomparision.java
@@ -0,0 +1,8 @@
+package com.brigelabz;
+
+public class linecomparision {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line Comparision computation program");
+
+    }
+}
